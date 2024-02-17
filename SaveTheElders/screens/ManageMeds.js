0fc49d3283e660e525012dd69ffd2ae9e92e61f8
Haijun Si <haijun@tufts-wifi-10-5-15-25.eecs.tufts.edu>
2024-02-17 15:31:00 -0500

    added a button

--- SaveTheElders/screens/ManageMeds.js
+++ SaveTheElders/screens/ManageMeds.js
@@ -1,11 +1,38 @@
-import React from 'react';
-import { Text, SafeAreaView} from 'react-native';
+import React, { useState } from 'react';
+import { View,Text, SafeAreaView, Button, StyleSheet} from 'react-native';
 
+
+const CounterButton = () => {
+    const [count, setCount] = useState(0);
+  
+    const increment = () => {
+      setCount(count + 1);
+    };
+  
+    const decrement = () => {
+      if (count > 0) {
+        setCount(count - 1);
+      }
+    };
+  
+    return (
+        <View style={styles.container}>
+          <View style={styles.buttonContainer}>
+            <Button title="+" onPress={increment} />
+            <Text style={styles.middleText}>{count}</Text>
+            <Button title="-" onPress={decrement} />
+          </View>
+        </View>
+      );
+  };
 export default function ManageMeds (){
     return (
         <SafeAreaView style={{ flex: 1, backgroundColor: '#E6D3CD' }} >
             {/* List view */}
-            <Text style={styles.title}>Todayis</Text>
+            <Text style={styles.title}>Today is</Text>
+            <CounterButton />
+
+
         </SafeAreaView>
     )
 }
@@ -14,5 +41,19 @@ const styles = StyleSheet.create({
         fontSize: 24,
         fontWeight: 'bold',
         marginBottom: 20,
-    }
+        marginLeft: 20,
+    },
+    container: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingVertical: 20,
+      },
+      buttonContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+      },
+      middleText: {
+        fontSize: 24,
+        marginHorizontal: 10,
+      },
 });
