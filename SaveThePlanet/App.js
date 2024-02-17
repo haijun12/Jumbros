04b9ff9f27a0e6c73e10d5d7cd0d088525eafe59
Charles Man <CharlieMan@MacBook-Pro-95.local>
2024-02-17 13:52:40 -0500

    fix

--- SaveThePlanet/App.js
+++ SaveThePlanet/App.js
@@ -1,11 +1,16 @@
+import * as React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { View, useWindowDimensions } from 'react-native';
+import { TabView, SceneMap } from 'react-native-tab-view';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <Text>This is Charlie</Text>
+      <Text> Onboarding!</Text>
       <StatusBar style="auto" />
     </View>
   );
